f9d02fbac91614ca41ae482090bfc6e8ff44dbb5
David Zulaica & Marc Lee <student@epicodus.com>
2015-01-14 10:12:44 -0800

    Add logic to app.rb file.

---
+++ app.rb
@@ -0,0 +1,24 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("./lib/**/*.rb")
+require("./lib/car_lot")
+
+get("/") do
+  @vehicles = Vehicle.all()
+  erb(:index)
+end
+
+post("/vehicle") do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  @vehicles = Vehicle.all()
+  erb(:index)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id"))
+  erb(:vehicle)
+end
